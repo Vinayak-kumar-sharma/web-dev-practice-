f08428ce7bfb7099e96360e3d929a600fb39e2fb
Vinayak-kumar-sharma <vinayaksharma07@yahoo.com>
2024-05-22 23:08:10 +0530

    counter

---
+++ CounterProgram/webindex.js
@@ -0,0 +1,34 @@
+/*
+>>>        Counter Program
+
+>>> This is a counter programmer related with html, css, javascript.
+
+*/
+
+
+
+const dec = document.getElementById("decreaseBtn")
+const reset = document.getElementById("resetBtn")
+const inc = document.getElementById("increaseBtn")
+const countlable = document.getElementById("countlable")
+let count = 0
+
+inc.onclick = function(){
+  if(count < 150){
+    count++
+    countlable.textContent = count;
+  }
+}
+dec.onclick = function(){
+if(count > -150){
+  count--
+
+  countlable.textContent = count;
+}
+  
+ 
+}
+reset.onclick = function(){
+  count = 0
+  countlable.textContent = count;
+}
